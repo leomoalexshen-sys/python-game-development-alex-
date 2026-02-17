09b50b5f38455217bb3bb9d0dca595567b69cad2
leomoalexshen-sys <leomoalexshen@gmail.com>
2026-02-17 17:57:33 +0100

    the first one

---
+++ game.py
@@ -0,0 +1,28 @@
+import pgzrun
+from random import randint
+TITLE="Good Shot"
+WIDTH=500
+HEIGHT = 500
+message = ""
+alien = Actor('alien')
+
+def draw():
+   screen.clear()
+   screen.fill (color = (128, 0, 0))
+   alien.draw()
+   screen.draw.text(message, center = (400, 0), fontsize= 30)
+
+def place_alien():
+   alien.x = randint (50, WIDTH-50)
+   alien.y = randint (50, WIDTH-50)
+
+def on_mouse_down (pos):
+    global message
+    if alien.collidepoint (pos):
+        message = "Good Shot"
+        place_alien ()
+    else:
+        message = "You missed"
+
+place_alien ()
+pgzrun.go ()
