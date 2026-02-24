c702be182c38bcba9ae81d2a1b09189b6554eaf0
leomoalexshen-sys <leomoalexshen@gmail.com>
2026-02-24 17:16:59 +0100

    Updated shoot the alien with the score

--- game.py
+++ game.py
@@ -1,28 +1,35 @@
 import pgzrun
 from random import randint
-TITLE="Good Shot"
-WIDTH=500
+
+TITLE = "Shoot the Alien"
+WIDTH = 500
 HEIGHT = 500
-message = ""
+
+message = "Click the Alien!"
+score = 0
+
 alien = Actor('alien')
 
 def draw():
-   screen.clear()
-   screen.fill (color = (128, 0, 0))
-   alien.draw()
-   screen.draw.text(message, center = (400, 0), fontsize= 30)
+    # global score
+    screen.clear()
+    screen.fill((128, 0, 0))
+    alien.draw()
+    screen.draw.text(message, center=(250, 30), fontsize=40, color="white")
+    screen.draw.text("Score:"+str(score), center=(250, 470), fontsize=35, color="yellow")
 
 def place_alien():
-   alien.x = randint (50, WIDTH-50)
-   alien.y = randint (50, WIDTH-50)
+    alien.x = randint(50, WIDTH - 50)
+    alien.y = randint(50, HEIGHT - 50)
 
-def on_mouse_down (pos):
-    global message
-    if alien.collidepoint (pos):
-        message = "Good Shot"
-        place_alien ()
+def on_mouse_down(pos):
+    global message, score
+    if alien.collidepoint(pos):
+        message = "Good Shot!"
+        score = score +1
+        place_alien()
     else:
-        message = "You missed"
+        message = "You Missed"
 
-place_alien ()
-pgzrun.go ()
+place_alien()
+pgzrun.go()
