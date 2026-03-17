cfd0a4bb47e9c2aa9ce2793835ca94d72d304e1b
leomoalexshen-sys <leomoalexshen@gmail.com>
2026-03-17 18:01:17 +0100

    bumble bee and rhe flower

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import pyzrun
+import pgzrun
 from random import randint
 
 width=600
@@ -6,23 +6,54 @@ height=500
 score=0
 game_over=False
 
-bee=Actor('bumble_bee')
+bee=Actor('bee')
 bee.pos=200, 200
 flower=Actor("flower")
 flower.pos=100, 100
 
-def update()
-    
+def update():
+    global score
 
-def draw()
+    if keyboard.left:
+        bee.x=bee.x-1
+
+    elif keyboard.right:
+        bee.x=bee.x+1
+
+    elif keyboard.up:
+        bee.y=bee.y-1
+
+    elif keyboard.down:
+        bee.y=bee.y+1
+
+    flower_collected=bee.colliderect(flower)
+    if flower_collected:
+        score=score+1
+        place_flower()
+
+def place_flower():
+    flower.x=randint(70, height-50)
+    flower.y=randint(70, height-50)
+
+def time_up():
+    global game_over
+    game_over = True
+
+timer=0
+
+def draw():
+    global timer
     screen.blit('background', (0,0))
     flower.draw()
-    bumble_bee.draw()
+    bee.draw()
     screen.draw.text('SCORE: '+str(score), color="black", midtop=(width/2, 10), fontsize=30)
+    screen.draw.text('time left: '+str(timer), color='black', midtop=(width/2, 50), fontsize=20)
+
     if game_over:
         screen.fill('black')
         screen.draw.text('TIME IS UP! YOUR FINAL SCORE IS: '+str(score), midtop=(width/2, 10), fontsize=50, color='red')
+    timer=timer+0.016666
 
-def place_flower():
-    flower.x=randint(width-50, height-50)
-    flower.y=randint(width-50, height-50)
+clock.schedule(time_up, 60.0)
+
+pgzrun.go()
